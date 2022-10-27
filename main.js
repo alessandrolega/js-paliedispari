b7412452948bbd4cc60c05115742dcb2ff644b3d
alessandrolega <alessandrolega24@gmail.com>
2022-10-27 15:40:09 +0200

    aggiunta palidroma

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
@@ -11,6 +13,22 @@
 
 
 // PALIDROMA
+
+let parola = prompt('Inserisci una parola');
+let reverse = '';
+
+for ( let i = parola.length; i >= 0; i--) {
+    if (i == parola) {
+        console.log('è palidroma')
+
+    } else {
+        console.log('non è palidroma')
+    }
+}
+
+
+
+
 
 
 
